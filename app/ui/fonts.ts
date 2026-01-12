2da823009abf9f3fe8536f04f258da640bcfb45c
Fabio Marsiaj <fabiobackend@gmail.com>
2026-01-12 11:51:43 -0300

    using class image to set both on desktop or mobile

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,10 +1,8 @@
-// Ajuste os nomes das fontes conforme o curso/tema que você está usando
 import { Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({
   subsets: ["latin"],
 });
-
 export const lusitana = Lusitana({
   weight: ["400", "700"],
   subsets: ["latin"],
