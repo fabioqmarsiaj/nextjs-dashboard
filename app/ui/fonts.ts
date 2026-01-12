9bdea8b264a999c85757c2350dbe254d56e42253
MARSIAJ Fabio (Consulting For Edenred) <fabio.marsiaj@consulting-for.edenred.com>
2026-01-12 11:02:44 -0300

    bootstrap project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+// Ajuste os nomes das fontes conforme o curso/tema que você está usando
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
